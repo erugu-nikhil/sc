f72a5c26e4426e19b5feed85df0d3e546ff7b632
Nikhil Erugu <you@example.com>
2025-02-14 05:38:21 -0800

    add msg

--- 05-Variables.sh
+++ 05-Variables.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 MOVIES=("PUSHPA" "RRR" "DEVARA")
 echo "first movies:$MOVIE[0]"
-echo "second movies:$MOVIE[0]"
-echo "third movies:$MOVIE[0]"
-echo "all movies:MOVIES[@]"
+echo "second movies:$MOVIE[1]"
+echo "third movies:$MOVIE[2]"
+echo "all movies:$MOVIES[@]"
