f82ba65f30df318318c2a40d78c2ed53bc700d1e
Nikhil Erugu <you@example.com>
2025-02-14 05:46:41 -0800

    add msg

--- 06-Variables.sh
+++ 06-Variables.sh
@@ -1,9 +1,9 @@
-All the variables passsed :$@
-number of variables :$#
-script name:$0
-present working directory:$PASSWORD
-home directory of current user:$HOME
-which user is running this script:$USER
-Process id of current script:$$
+echo " All the variables passsed :$@"
+echo "chonumber of variables :$#"
+echo "script name:$0"
+echo "present working directory:$PASSWORD"
+echo "home directory of current user:$HOME"
+echo "which "user is running this script:$USER"
+echo "Process id of current script:$$"
 cd
-Process id of last command in background :$1
+echo "Process id of last command in background :$1"
